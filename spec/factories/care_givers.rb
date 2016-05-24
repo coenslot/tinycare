56562658a9b7aeea4770e0cf9ba8130f2adcec60
coenslot <coenslot@hotmail.com>
2016-05-24 15:19:55 +0200

    changed model Care_user to Care_giver

---
+++ spec/factories/care_givers.rb
@@ -0,0 +1,11 @@
+FactoryGirl.define do
+  factory :care_giver do
+    email "MyString"
+    first_name "MyString"
+    insertion "MyString"
+    last_name "MyString"
+    telephone 1
+    admin false
+    password "MyString"
+  end
+end
