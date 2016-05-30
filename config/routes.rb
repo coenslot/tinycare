95bb579bae1b8c2af93494c0fd0ea9f82423e8ef
Kevin Etore <info@etore.nl>
2016-05-30 14:54:24 +0200

    changed routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,22 @@ Rails.application.routes.draw do
   devise_for :care_givers
   devise_for :fitbit_users, :controllers => { :omniauth_callbacks => "callbacks" }
 
+  devise_scope :care_giver do
+    authenticated :care_giver do
+      root :to => 'care_givers#client_overview'
+    end
+    unauthenticated :user do
+      root :to => 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
+
   #Fitbit_api
-  get '/fitbit_api/dashboard', to: 'fitbit_api#dashboard' 					#Client notification 
+  get '/fitbit_api/dashboard', to: 'fitbit_api#dashboard' 					#Client notification
   get '/fitbit_api/profile', to: 'fitbit_api#profile'						#Client profile
   get '/fitbit_api/data', to: 'fitbit_api#data'								#All client data
   get '/fitbit_api/notifications', to: 'fitbit_api#notifications'			#All notifications
   get '/fitbit_api/send_notification', to: 'fitbit_api#send_notification'	#Send a notification to stakeholder
- 
+
 
   #Care_givers
   get '/care_givers/client_overview', to: 'care_givers#client_overview'		#Client overview
