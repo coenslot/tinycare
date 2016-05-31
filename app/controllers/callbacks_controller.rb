81322d0fae890dc53a6d5b742183abd0acd48a17
Kevin Etore <info@etore.nl>
2016-06-01 00:22:40 +0200

    linked fitbit user with care giver

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -1,6 +1,8 @@
 class CallbacksController < ApplicationController
+  before_action :authenticate_care_giver!
+
   def fitbit_oauth2
-    @fitbit_user = FitbitUser.from_omniauth(request.env["omniauth.auth"])
+    @fitbit_user = FitbitUser.from_omniauth(request.env["omniauth.auth"], current_care_giver)
     sign_in_and_redirect @fitbit_user
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160526144435) do
+ActiveRecord::Schema.define(version: 20160531091520) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 20160526144435) do
     t.string   "city"
     t.date     "birth"
     t.string   "avatar"
+    t.integer  "care_giver_id"
     t.index ["email"], name: "index_fitbit_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_fitbit_users_on_reset_password_token", unique: true, using: :btree
   end
