44c08b0cc3227e74958be3f1b78e8157e847a973
Kevin Etore <info@etore.nl>
2016-06-02 14:14:49 +0200

    fixed redirect link after fitbit user signs in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def sign_in_and_redirect_to(resource, path)
+    sign_in(resource)
+    redirect_to path
+  end
 end
--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -3,7 +3,7 @@ class CallbacksController < ApplicationController
 
   def fitbit_oauth2
     @fitbit_user = FitbitUser.from_omniauth(request.env["omniauth.auth"], current_care_giver)
-    sign_in_and_redirect @fitbit_user
+    sign_in_and_redirect_to(@fitbit_user, fitbit_api_dashboard_path)
   end
 
   def failure
